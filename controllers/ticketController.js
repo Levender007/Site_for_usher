e45d0d91a00c2056e29d7798f2cc3694353bd0d5
Levender007 <fareshn@mail.ru>
2025-02-03 18:59:00 +0300

    fixup! Ticket Page

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,8 +1,11 @@
-const {homeWay} = require('../conf.js');
+const {homeWay, apiWay, apiSessions, apiTickets} = require('../conf.js');
 
 exports.homePage = function (req, res) {
     res.render('ticketPage.hbs', {
         homeWay: homeWay,
+        apWay: apiWay,
+        apiSessions: apiSessions,
+        apiTickets: apiTickets,
         seansID: req.params.seansID
     });
 }
