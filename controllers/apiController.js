25dd812939a12e29acd5eed40238a525f9bb8aba
Levender007 <fareshn@mail.ru>
2025-02-06 21:42:55 +0300

    Added some stuff

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,5 +1,9 @@
 const mysql = require("mysql2");
-const {dbConnect, M} = require('../conf.js');
+const {dbConnect, M, N} = require('../conf.js');
+
+function TooMuchInOneHands(tickets) {
+    return tickets > N;
+}
 
 async function SessionsHasIntersection (date, duration, hallID, sessionID = 0) {
     const conn = mysql.createConnection(dbConnect).promise();
@@ -10,6 +14,53 @@ async function SessionsHasIntersection (date, duration, hallID, sessionID = 0) {
     await conn.query(sql, param)
         .then(([result, _]) => {
             res = result[0]["s"] > 0;
+        })
+        .catch((err) => {
+            console.log(err);
+            res = "Error";
+        });
+    return res;
+}
+
+async function NoSpaceInHall(seansID, tickets, id = "0", invertOldTickets = false) {
+    const conn = mysql.createConnection(dbConnect).promise();
+    let oldTickets = 0;
+    if(id !== "0") {
+        const fsql = "SELECT Tickets FROM reservations WHERE ID = ?";
+        const fparam = [id];
+        await conn.query(fsql, fparam)
+            .then(([result, _]) => {
+                oldTickets = result[0]["Tickets"];
+                if(invertOldTickets)
+                    oldTickets *= -1;
+            })
+            .catch((err) => {
+                console.log(err);
+                return "Error";
+            })
+    }
+    const sql = "SELECT Capacity, IFNULL(SUM(Tickets), 0) as s FROM reservations RIGHT JOIN sessions ON sessions.ID = reservations.SessionID LEFT JOIN halls ON halls.ID = sessions.HallID WHERE sessions.ID = ?";
+    const param = [seansID];
+    let res = 0;
+    await conn.query(sql, param)
+        .then(([result, _]) => {
+            res = parseInt(result[0]["Capacity"]) < result[0]["s"] + tickets - oldTickets;
+        })
+        .catch((err) => {
+            console.log(err);
+            res = "Error";
+        })
+    return res;
+}
+
+async function NewHallHasNoSpace (seansID, hallID) {
+    const conn = mysql.createConnection(dbConnect).promise();
+    const sql = "SELECT (SELECT Capacity FROM halls WHERE ID = ?) - IFNULL(SUM(Tickets), 0) as s FROM sessions LEFT JOIN reservations ON reservations.SessionID = sessions.ID WHERE sessions.ID = ?";
+    const param = [hallID, seansID];
+    let res = 0;
+    await conn.query(sql, param)
+        .then(([result, _]) => {
+            res = result[0]["s"] < 0;
         })
         .catch((err) => {
             console.log(err);
@@ -47,6 +98,15 @@ exports.UpdateSession = async function (req, res) {
         res.send("HasIntersection");
         return;
     }
+    const NoSpace =  await NewHallHasNoSpace(req.params.id, req.body.hallID);
+    if(NoSpace === "Error") {
+        res.status(500).send("New hall space Check Error");
+        return;
+    }
+    else if(NoSpace) {
+        res.send("NewHallHasNoSpace");
+        return;
+    }
     const conn = mysql.createConnection(dbConnect).promise();
     const sql = "UPDATE sessions SET Film = ?, Date = ?, Duration = ?, HallID = ? WHERE ID = ?";
     const param = [req.body.film, req.body.date, req.body.duration, req.body.hallID, req.params.id];
@@ -76,8 +136,8 @@ exports.CreateSession = async function (req, res) {
         return;
     }
     const conn = mysql.createConnection(dbConnect).promise();
-    const sql = "INSERT INTO sessions (Film, Date, Duration, HallID, UnsoldTickets) VALUES (?, ?, ?, ?, (SELECT Capacity FROM halls WHERE ID = ?))";
-    const param = [req.body.film, req.body.date, req.body.duration, req.body.hallID, req.body.hallID];
+    const sql = "INSERT INTO sessions (Film, Date, Duration, HallID) VALUES (?, ?, ?, ?)";
+    const param = [req.body.film, req.body.date, req.body.duration, req.body.hallID];
     conn.query(sql, param)
         .then(([result, _]) => {
             if (result["affectedRows"] > 0) {
@@ -131,7 +191,7 @@ exports.GetSession = function (req, res) {
 exports.GetSessions = function (req, res) {
     const conn = mysql.createConnection(dbConnect).promise();
     const DateTime = new Date(req.params.date);
-    const sql = "SELECT * FROM sessions WHERE Date > '?-?-? ?:?:?' ORDER BY Date, HallID";
+    const sql = "SELECT sessions.*, Capacity - IFNULL(SUM(Tickets), 0) as UnsoldTickets FROM sessions LEFT JOIN halls ON halls.ID = sessions.HallID LEFT JOIN reservations ON reservations.SessionID = sessions.ID WHERE Date > '?-?-? ?:?:?' GROUP BY sessions.ID ORDER BY Date, HallID";
     const date = [DateTime.getFullYear(), DateTime.getMonth() + 1, DateTime.getDate(), DateTime.getHours(), DateTime.getMinutes(), DateTime.getSeconds()];
     conn.query(sql, date)
         .then(result => {
@@ -175,7 +235,20 @@ exports.DeleteTicket = function (req, res) {
         });
 }
 
-exports.UpdateTicket = function (req, res) {
+exports.UpdateTicket = async function (req, res) {
+    if(TooMuchInOneHands(req.body.ticketsCount)) {
+        res.send("TooMuchInOneHands");
+        return;
+    }
+    const NoSpace =  await NoSpaceInHall(req.body.seansID, req.body.ticketsCount, req.params.id);
+    if(NoSpace === "Error") {
+        res.status(500).send("Hall space Check Error");
+        return;
+    }
+    else if(NoSpace) {
+        res.send("NoSpaceInHall");
+        return;
+    }
     const conn = mysql.createConnection(dbConnect).promise();
     const sql = "UPDATE reservations SET Tickets = ?, FIO = ? WHERE ID = ?";
     const param = [req.body.ticketsCount, req.body.fio, req.params.id];
@@ -194,13 +267,30 @@ exports.UpdateTicket = function (req, res) {
         });
 }
 
-exports.CreateTicket = function (req, res) {
+exports.CreateTicket = async function (req, res) {
+    if(TooMuchInOneHands(req.body.ticketsCount)) {
+        res.send("TooMuchInOneHands");
+        return;
+    }
+    const NoSpace =  await NoSpaceInHall(req.body.seansID, req.body.ticketsCount);
+    if(NoSpace === "Error") {
+        res.status(500).send("Hall space Check Error");
+        return;
+    }
+    else if(NoSpace) {
+        res.send("NoSpaceInHall");
+        return;
+    }
     const conn = mysql.createConnection(dbConnect).promise();
-    let sql = "SELECT ID FROM reservations WHERE SessionID = ? and FIO = ?";
+    let sql = "SELECT ID, Tickets FROM reservations WHERE SessionID = ? and FIO = ?";
     let param = [req.body.seansID, req.body.fio];
     conn.query(sql, param)
         .then(([result, _]) => {
             if (result.length > 0) {
+                if(TooMuchInOneHands(parseInt(req.body.ticketsCount) + parseInt(result[0]["Tickets"]))) {
+                    res.send("TooMuchInOneHands");
+                    return;
+                }
                 sql = "UPDATE reservations SET Tickets = Tickets + ? WHERE ID = ?";
                 param[0] = req.body.ticketsCount;
                 param[1] = result[0]["ID"];
@@ -277,7 +367,16 @@ exports.GetTickets = function (req, res) {
         });
 }
 
-exports.RebaseTicket = function (req, res) {
+exports.RebaseTicket =  async function (req, res) {
+    const NoSpace =  await NoSpaceInHall(req.params.newSeans, 0, req.params.id, true);
+    if(NoSpace === "Error") {
+        res.status(500).send("Hall space Check Error");
+        return;
+    }
+    else if(NoSpace) {
+        res.send("NoSpaceInHall");
+        return;
+    }
     const conn = mysql.createConnection(dbConnect).promise();
     const sql = "UPDATE reservations SET SessionID = ? WHERE ID = ?";
     const param = [req.params.newSeans, req.params.id];
